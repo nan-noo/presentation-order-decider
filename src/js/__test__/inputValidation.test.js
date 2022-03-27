3df3d5b59e2f194450045cd6417cc91f17c1875c
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-03-27 23:01:12 +0900

    test: 순서 입력값 유효성 검사

--- src/js/__test__/inputValidation.test.js
+++ src/js/__test__/inputValidation.test.js
@@ -1,4 +1,4 @@
-import { validatePrevInfo } from '../validator.js';
+import { validateOrderList, validatePrevInfo } from '../validator.js';
 
 describe('사전 정보 입력값 유효성 테스트', () => {
   test('사전 정보 입력값은 모두 공백이 아니어야 한다.', () => {
@@ -55,6 +55,35 @@ describe('사전 정보 입력값 유효성 테스트', () => {
     };
     expect(() => validatePrevInfo(prevInfo)).toThrow(
       `총 발표 순서는 ${prevInfo.totalTeamCount}개 이상, 100개 이하여야 합니다.`
+    );
+  });
+});
+
+describe('원하는 순서 입력값 유효성 검사', () => {
+  const prevInfo = {
+    totalTeamCount: 5,
+    totalPriorities: 3,
+    totalOrder: 10,
+  };
+
+  test('공백은 없어야 한다.', () => {
+    const orderList = [1, NaN, 3];
+    expect(() => validateOrderList(orderList, prevInfo.totalOrder)).toThrow(
+      '공백 없이 모두 입력해주세요.'
+    );
+  });
+
+  test('중복된 숫자는 없어야 한다.', () => {
+    const orderList = [1, 1, 3];
+    expect(() => validateOrderList(orderList, prevInfo.totalOrder)).toThrow(
+      '중복 없이 입력해주세요.'
+    );
+  });
+
+  test('1이상, 총 발표 순서 이하의 값을 입력해야 한다.', () => {
+    const orderList = [1, 2, 11];
+    expect(() => validateOrderList(orderList, prevInfo.totalOrder)).toThrow(
+      `1 이상, ${prevInfo.totalOrder} 이하의 값을 입력해야 합니다.`
     );
   });
 });
--- src/js/validator.js
+++ src/js/validator.js
@@ -15,3 +15,17 @@ export const validatePrevInfo = ({ totalTeamCount, totalPriorities, totalOrder }
     throw new Error(`총 발표 순서는 ${totalTeamCount}개 이상, 100개 이하여야 합니다.`);
   }
 };
+
+export const validateOrderList = (orderList, totalOrder) => {
+  if (orderList.some((order) => !order)) {
+    throw new Error('공백 없이 모두 입력해주세요.');
+  }
+
+  if (new Set(orderList).size !== orderList.length) {
+    throw new Error('중복 없이 입력해주세요.');
+  }
+
+  if (orderList.some((order) => order > totalOrder)) {
+    throw new Error(`1 이상, ${totalOrder} 이하의 값을 입력해야 합니다.`);
+  }
+};
