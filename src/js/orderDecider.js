694ffba552291ad299109090af7a7e648c2adcbf
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-11 18:41:21 +0900

    refactor: orderDecider.js
    순서 결정 메서드 리팩토링: early return 및 로직 수정

--- src/js/orderDecider.js
+++ src/js/orderDecider.js
@@ -14,44 +14,57 @@ class OrderDecider {
       if (!remainedTeam.length) return false;
 
       const candidateTeamByOrder = this.#sortTeamByOrder(orderListByPriority, remainedTeam);
-      this.#assignOrderToEachTeam(candidateTeamByOrder, remainedTeam);
+      remainedTeam = this.#assignOrderToEachTeam(candidateTeamByOrder, remainedTeam);
+      return true;
     });
   }
 
   #sortTeamByOrder(orderListByPriority, remainedTeam) {
     const candidateTeamByOrder = {};
+
     orderListByPriority.every((order, index) => {
       const teamNumber = index + 1;
-      if (remainedTeam.includes(teamNumber) && !Object.values(this.orderResult).includes(order)) {
-        if (!candidateTeamByOrder[order]) {
-          candidateTeamByOrder[order] = [teamNumber];
-          return true;
-        }
-        candidateTeamByOrder[order].push(teamNumber);
+
+      if (!remainedTeam.includes(teamNumber) || Object.values(this.orderResult).includes(order)) {
+        return true;
       }
+
+      if (candidateTeamByOrder[order]) {
+        candidateTeamByOrder[order].push(teamNumber);
+        return true;
+      }
+      candidateTeamByOrder[order] = [teamNumber];
       return true;
     });
+
     return candidateTeamByOrder;
   }
 
   #assignOrderToEachTeam(candidateTeamByOrder, remainedTeam) {
+    let newRemainedTeam = remainedTeam;
+
     Object.entries(candidateTeamByOrder).every(([order, candidateTeams]) => {
       const orderNumber = Number(order);
-      if (candidateTeams.length === 1) {
-        this.orderResult[candidateTeams[0]] = orderNumber;
-        remainedTeam = remainedTeam.filter((team) => team !== candidateTeams[0]);
-        return true;
+      let winnerTeam = candidateTeams[0];
+
+      if (candidateTeams.length > 1) {
+        winnerTeam = pickRandomElementInArray(candidateTeams);
       }
-      const winnerTeam = pickRandomElementInArray(candidateTeams);
+
       this.orderResult[winnerTeam] = orderNumber;
-      remainedTeam = remainedTeam.filter((team) => team !== winnerTeam);
+      newRemainedTeam = newRemainedTeam.filter((team) => team !== winnerTeam);
       return true;
     });
+
+    return newRemainedTeam;
   }
 
   #initOrderResult(teamCount) {
     const result = {};
-    Array.from({ length: teamCount }, (_, team) => (result[team + 1] = NOT_ASSIGNED_TEXT));
+    Array.from({ length: teamCount }, (_, team) => {
+      result[team + 1] = NOT_ASSIGNED_TEXT;
+      return '';
+    });
     return result;
   }
 }
