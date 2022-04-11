4eecf0a3d1d674bf0ecc99f9d287bbc4b845b2f6
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-04-11 17:52:50 +0900

    refactor: validator 함수 분리

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,26 +1,32 @@
 import { ORDER_LIST_ERROR_MESSAGE, PREV_INFO_ERROR_MESSAGE } from './constant/errorMessage';
 import { PREV_INFO_RULE } from './constant/rule';
 
+const isBlankInput = (totalTeamCount, totalPriorities, totalOrder) =>
+  !totalTeamCount || !totalPriorities || !totalOrder;
+
+const isOutOfTeamRange = (totalTeamCount) =>
+  totalTeamCount < PREV_INFO_RULE.MIN_TEAM_COUNT || totalTeamCount > PREV_INFO_RULE.MAX_TEAM_COUNT;
+
+const isOutOfPriorityRange = (totalPriorities) =>
+  totalPriorities < PREV_INFO_RULE.MIN_PRIORITY || totalPriorities > PREV_INFO_RULE.MAX_PRIORITY;
+
+const isOutOfOrderRange = (totalOrder, totalTeamCount) =>
+  totalOrder < totalTeamCount || totalOrder > PREV_INFO_RULE.MAX_ORDER;
+
 export const validatePrevInfo = ({ totalTeamCount, totalPriorities, totalOrder }) => {
-  if (!totalTeamCount || !totalPriorities || !totalOrder) {
+  if (isBlankInput(totalTeamCount, totalPriorities, totalOrder)) {
     throw new Error(PREV_INFO_ERROR_MESSAGE.NO_EMPTY_INPUT_ALLOWED);
   }
 
-  if (
-    totalTeamCount < PREV_INFO_RULE.MIN_TEAM_COUNT ||
-    totalTeamCount > PREV_INFO_RULE.MAX_TEAM_COUNT
-  ) {
+  if (isOutOfTeamRange(totalTeamCount)) {
     throw new Error(PREV_INFO_ERROR_MESSAGE.OUT_OF_TEAM_COUNT_RANGE);
   }
 
-  if (
-    totalPriorities < PREV_INFO_RULE.MIN_PRIORITY ||
-    totalPriorities > PREV_INFO_RULE.MAX_PRIORITY
-  ) {
+  if (isOutOfPriorityRange(totalPriorities)) {
     throw new Error(PREV_INFO_ERROR_MESSAGE.OUT_OF_PRIORITY_RANGE);
   }
 
-  if (totalOrder < totalTeamCount || totalOrder > PREV_INFO_RULE.MAX_ORDER) {
+  if (isOutOfOrderRange(totalOrder, totalTeamCount)) {
     throw new Error(PREV_INFO_ERROR_MESSAGE.OUT_OF_ORDER_RANGE(totalTeamCount));
   }
 };
